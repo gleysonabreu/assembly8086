d47fe3e389d16efab55a4cd0e684f7480a28324e
gleysonabreu <gleyson_datu@hotmail.com>
2018-11-16 23:08:31 -0300

    Programas funcionando com porem

--- calc_dividir.asm
+++ calc_dividir.asm
@@ -1,39 +1,38 @@
- ORG 100h
+ORG 100h
 .MODEL small
 .STACK 512d
 .DATA 
-num1 dw 6
-num2 dw 2
+num1 dw 18 ; dividendo
+num2 dw 2 ; deivisor
+count dw ?
 
-.CODE 
+.CODE   
 
-    MOV CX, num1
-    mov ax, num1   ;; calculando
-    
-    
-           
-    
-laco:
+        mov cx, num1
+ 
+        mov ax, num1
+
+calcula:
+    dec cx  ;; Decrementa o cx.
      
-    aam
+    sub ax, num2  ;; faz a subtracao do numero
     
-    add ax, 3030h
+    inc count    ;; Incrementa quantas vezes o programa e executado.
     
-    mov dh,al
-    mov dl,ah
-    
-    mov ah,02h
-    mov dl,dh
-    mov ah,02h
      
-     sub ax, num2
-    
-      
-      dec cx
-      
-    
-    INT 021h 
-    
-LOOP laco 
-
-ret 
+LOOP calcula ;; Executa o loop
+              
+            aam              ;; 
+                               ;   Fazem a conversao para decimal
+            add count, 3030h ;;
+            
+            
+            mov dx, count ;;
+            mov ah, 2h    ;; Mostrar o resultado
+            int 21h       ;;
+            
+            
+            
+        
+            
+   hlt
--- calc_multiplicar.asm
+++ calc_multiplicar.asm
@@ -1,39 +1,28 @@
-#MAKE_COM#
 ORG 100h
 .MODEL small
 .STACK 512d
 .DATA 
-num1 dw 10 ; multiplicando
+num1 dw 40 ; multiplicando
 num2 dw 2 ; multiplicador
 
 .CODE
     
-    MOV CX,  num1
-    mov ax, num2   ;; calculando 
-     
+        MOV CX, num1  ; quantas vezes o loop vai rodar   
     
+calcula: 
+    add ax, num2  ; faz a adicao do numero     
     
-    aam
-    
-    add ax, 3030h
-    
-    mov dh,al
-    mov dl,ah
-    
-    mov ah,02h
-    mov dl,dh
-    mov ah,02h
-     
-    
-laco: 
-    add ax, num2
-    
-    
-    INT 021h
-    
-    
-       
-    
-LOOP laco  
+LOOP calcula
 
-ret
+            aam           ;; 
+                            ; Fazem a conversao para decimal
+            add ax, 3030h ;; 
+            
+            mov dh,al
+            mov dl,ah
+            mov ah,02h
+            int 21h
+            
+            mov dl,dh
+            mov ah,02h
+            int 21h
